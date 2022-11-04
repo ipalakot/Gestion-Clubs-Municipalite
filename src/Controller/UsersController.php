b5f502e377cdc0a65bc0cbf6f6d05c644c8809f9
GillesFromGuinot <glevy@guinot.asso.fr>
2022-11-04 11:52:11 +0100

    Listing

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Auteur;
+use App\Repository\AuteurRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\Common\Persistence\ObjectManager;
 
 class UsersController extends AbstractController
 {
     /**
      * @Route("/users", name="app_users")
      */
-    public function index(): Response
+    public function index(AuteurRepository $auteurRepository): Response
     {
         return $this->render('users/index.html.twig', [
-            'controller_name' => 'UsersController',
+            'auteurs' => $auteurRepository->findAll(),
         ]);
     }
 }
