d82441f6a7c1c5e696d1688cfeba42f61921fc56
ipalakot <ipalakot@gmail.com>
2022-12-15 12:19:44 +0100

    Serializer

---
+++ migrations/Version20221215102547.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221215102547 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD noms VARCHAR(155) DEFAULT NULL, ADD prenoms VARCHAR(155) DEFAULT NULL, ADD login VARCHAR(155) DEFAULT NULL, ADD image_name VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP noms, DROP prenoms, DROP login, DROP image_name');
+    }
+}
--- src/Controller/ApiArticleController.php
+++ src/Controller/ApiArticleController.php
@@ -7,74 +7,92 @@ use FOS\UserBundle\Model\Group;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\Transport\Serialization\Serializer;
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * @Route("/api", name="api_")
- * 
+ *
  */
 class ApiArticleController extends AbstractController
 {
     /**
      * @Route("/", name="article", methods={"GET"}) // J'affiche ici la liste des methodes
      */
-    public function liste(ArticleRepository $articleRepo, NormalizerInterface $normalizer): Response
+    public function liste(ArticleRepository $articleRepo, SerializerInterface $serializer): Response
     {
         #_1 Recupération des Articles
-            // On récupère la liste des articles et on les affiche comme nous savons dejà faire
-            // $article = $articleRepo -> findAll();
-            //dd($article);
+        // On récupère la liste des articles et on les affiche comme nous savons dejà faire
+        // $article = $articleRepo -> findAll();
+        //dd($article);
 
        
-         #_2 Json_Encode 
-            // j'utilise du Json pour l'affichage de mon tableau
-            //  Je vais utiliser l'encodeur JSON1 l'affiche du Tableau
+        #_2 Json_Encode
+        // j'utilise du Json pour l'affichage de mon tableau
+        //  Je vais utiliser l'encodeur JSON1 l'affiche du Tableau
                         
-           /* $testJson = json_encode([
-                'Test1 ' => 'Sport et action publique locale1 \\n\\n',
-                'Test2' => 'Guinot Dance School, Cours de danse et Ecole',
-                'Test3' => 'Cours de musique et de chant à domicile !!',
-                'Test4' => 'Atelier Yoga – Association Loi 1901 dans la commune'
-            ]); */
+        /* $testJson = json_encode([
+             'Test1 ' => 'Sport et action publique locale1 \\n\\n',
+             'Test2' => 'Guinot Dance School, Cours de danse et Ecole',
+             'Test3' => 'Cours de musique et de chant à domicile !!',
+             'Test4' => 'Atelier Yoga – Association Loi 1901 dans la commune'
+         ]); */
 
-            //$article = $articleRepo -> findAll();
-            //$articlejson = json_encode($article);
-            //dd($articlejson);
+        //$article = $articleRepo -> findAll();
+        //$articlejson = json_encode($article);
+        //dd($articlejson);
         
         #_3 Utiliser le Normalizer
-            //$articles = $articleRepo -> findAll();
-            //$articlesnormalises = $normalizer -> normalize($articles);
-            // dd( $articlesnormalises);
+        //$articles = $articleRepo -> findAll();
+        //$articlesnormalises = $normalizer -> normalize($articles);
+        // dd( $articlesnormalises);
         
 
         #_4 Tagger les arttibuts dont a besoin
-
-            $articles = $articleRepo -> findAll();
-            $articlNormalises = $normalizer->normalize($articles, null, [
-                    'groups'=>'article:api'
-                ]);
-            //dd($articlNormalises);
+        $articles = $articleRepo -> findAll();
+        /* $articlNormalises = $normalizer->normalize($articles, null, [
+                     'groups'=>'article:api'
+                 ]);*/
+        //dd($articlNormalises);
         
-        #_5 Encode Article avec Json_Encode 
-            $articlesJson = json_encode($articlNormalises);
-            dd($articlesJson);
+        #_5 Encode Article avec Json_Encode
+        //$articlesJson = json_encode($articlNormalises);
+        //dd($articlesJson);
             
-
-
-       /* $response = new Response();
-        $response -> headers->set('Content-Type', 'application/json');
+        #_6 Retourner le Tableau avec une reponse HTTP
+        // creer une nouvelle Response
+        //Passer $articlesJson
+        // Utiliser 1 Content-Type pour signifier que l'application va s'afficher en Json
         
-        return $this->json($articleRepo->findAll(), 200, [], [
-            "groups"=> "article:api"],
-        ); */
+        //$articlesJson = json_encode($articlNormalises);
+        /*$response = new Response($articlesJson, 200, [
+            "content-type"=>"application/json"
+         ]);
+
+        return $response; */
+
+        #_6 La serialization
+        //Regrouper les #3 & #5
+        // Le Serializer embarque avec lui Le Normalizer et l'Encodage
+
+        $articleSerialises  =$serializer->serialize($articles, 'json', [
+                                    'groups'=> 'article:api']);
+            
+        $response = new Response($articleSerialises, 200, [], true);
+                
+        return $response;
+
+
+
+        /* $response = new Response();
+         $response -> headers->set('Content-Type', 'application/json');
+
+         return $this->json($articleRepo->findAll(), 200, [], [
+             "groups"=> "article:api"],
+         ); */
         
         #_3  utiliser le TAG pour choisir les attributs que l'on veut afficher
-        
-
-
-
-
-        
     }
 }
