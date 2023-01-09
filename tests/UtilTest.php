f272ed698354b175cce108243175a01b0d6201d1
Ipalakot <ipalakot@gmail.com>
2023-01-09 13:59:54 +0100

    Php Unit Test

---
+++ tests/UtilTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UtilTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+
+            // $this->assertTrue(true);
+                //$this->assertTrue(true);
+                $this->assertEquals(5, 2*2); 
+                $this->assertEquals(12, 7+5);
+    }
+}
