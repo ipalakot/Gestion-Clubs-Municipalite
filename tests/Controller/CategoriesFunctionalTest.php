2d39bc70f0b39cb4c842a84a3569b08da051d6f2
Ipalakot <ipalakot@gmail.com>
2023-01-16 13:39:26 +0100

    Functionnal test Page / List

--- tests/Controller/CategoriesFunctionalTest.php
+++ tests/Controller/CategoriesFunctionalTest.php
@@ -9,10 +9,27 @@ class CategoriesFunctionalTest extends WebTestCase
     public function testIndexcategories(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', 'categorie/');
+        $client->request('GET', 'categorie/'); //Pas besoin de Scrawler
 
-        //$this->assertResponseIsSuccessful();
+        $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
        // $this->assertclsertSelectorTextContains('', '');
+    }
+
+    public function testListCategorie(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', 'categorie/'); // utilisation du scrawler
+        $this->assertclsertSelectorTextContains('h1', 'Liste des Categories');
+    }
+
+
+    public function testNewcategories(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', 'categorie/new');
+
+        $this->assertResponseIsSuccessful();
+       // $this->assertSelectorTextContains('', '');
     }
 }
